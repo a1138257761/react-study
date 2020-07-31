8b2cfab41af61dcb295e3bc7ccf00912db4979f0
SuZhe <37095212+a1138257761@users.noreply.github.com>
2020-07-31 11:17:38 +0800

    summary: setState注意事项

--- src/pages/IfDemo/IfDemo.jsx
+++ src/pages/IfDemo/IfDemo.jsx
@@ -15,6 +15,38 @@ export default class IfDemo extends React.Component {
     }
   }
 
+  deepClone = (target) => {
+    let result;
+    // 如果当前需要深拷贝的是一个对象的话
+    if (typeof target === 'object') {
+    // 如果是一个数组的话
+      if (Array.isArray(target)) {
+        result = []; // 将result赋值为一个数组，并且执行遍历
+        for (let i in target) {
+            // 递归克隆数组中的每一项
+            result.push(this.deepClone(target[i]))
+        }
+      // 判断如果当前的值是null的话；直接赋值为null
+      } else if(target===null) {
+        result = null;
+      // 判断如果当前的值是一个RegExp对象的话，直接赋值    
+      } else if(target.constructor===RegExp){
+        result = target;
+      }else {
+      // 否则是普通对象，直接for in循环，递归赋值对象的所有值
+        result = {};
+        for (let i in target) {
+          result[i] = this.deepClone(target[i]);
+        }
+      }
+    // 如果不是对象的话，就是基本数据类型，那么直接赋值
+    } else {
+        result = target;
+    }
+    // 返回最终结果
+    return result;
+  }
+
   changeStatus = () => {
     this.setState({
       flag: !this.state.flag
@@ -33,13 +65,17 @@ export default class IfDemo extends React.Component {
   }
 
   addPerson = () => {
+    let l = this.deepClone(this.state.lists)
+    l.push({ name: `person${this.state.lists.length + 1}`, age: parseInt(Math.random() * 30, 10) })
     this.setState({
-      list: this.state.lists.push({ name: `person${this.state.lists.length + 1}`, age: parseInt(Math.random() * 30, 10) })
+      lists: l
     })
   }
   reducePerson = () => {
+    let l = this.deepClone(this.state.lists)
+    l.pop();
     this.setState({
-      list: this.state.lists.pop()
+      lists: l
     })
   }
 
